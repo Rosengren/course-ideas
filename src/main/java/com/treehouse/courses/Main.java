421f899111a16203a60f7a0f0bf28a2469620f2e
Rosengren <krosen@amazon.com>
2016-07-27 11:43:13 -0700

    removed sign-in template

--- src/main/java/com/treehouse/courses/Main.java
+++ src/main/java/com/treehouse/courses/Main.java
@@ -13,6 +13,11 @@ import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.Spark.staticFileLocation;
 
+
+/*
+ * Default local host:
+ * http://localhost:4567/
+ */
 public class Main {
 
     public static void main(String[] args) {
