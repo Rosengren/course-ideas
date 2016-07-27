038ef489cdbb84deb1bc48e5d14d8c115501f0a0
Rosengren <krosen@amazon.com>
2016-07-27 11:41:41 -0700

    added css and have sign-in page redirect to homepage

--- src/main/java/com/treehouse/courses/Main.java
+++ src/main/java/com/treehouse/courses/Main.java
@@ -1,5 +1,6 @@
 package com.treehouse.courses;
 
+import com.treehouse.courses.model.CourseIdea;
 import com.treehouse.courses.model.CourseIdeaDAO;
 import com.treehouse.courses.model.SimpleCourseIdeaDAO;
 import spark.ModelAndView;
@@ -32,7 +33,24 @@ public class Main {
             String username = req.queryParams("username");
             res.cookie("username", username);
             model.put("username", username);
-            return new ModelAndView(model, "sign-in.hbs");
+            res.redirect("/");
+            return null;
+        });
+
+        get("/ideas", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("ideas", dao.findAll());
+            return new ModelAndView(model, "ideas.hbs");
         }, new HandlebarsTemplateEngine());
+
+        post("/ideas", (req, res) -> {
+            String title = req.queryParams("title");
+            // TODO:csd - This username is tied to the cookie implementation
+            CourseIdea courseIdea = new CourseIdea(title,
+                    req.cookie("username"));
+            dao.add(courseIdea);
+            res.redirect("/ideas");
+            return null;
+        });
     }
 }
